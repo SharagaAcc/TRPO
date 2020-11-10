01b4e38fa5265c329f529a3749ecbb856cbef904
Adeo <Adeo@mail.ru>
2020-11-10 16:52:04 +0300

    2

---
+++ lab-06.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS list;
+CREATE TABLE list (name TEXT, value INT, image TEXT);
+
+INSERT INTO list VALUES ('Apple'      , 10  , 'img-apple.png      ');
+INSERT INTO list VALUES ('Blueberry'  , 100 , 'img-blueberry.png  ');
+INSERT INTO list VALUES ('Cherry'     , 30  , 'img-cherry         ');
+INSERT INTO list VALUES ('Grapes'     , 70  , 'img-Grapes         ');
+INSERT INTO list VALUES ('Lemon'      , 3   , 'img-Lemon          ');
+INSERT INTO list VALUES ('Orange'     , 5   , 'img-Orange         ');
+INSERT INTO list VALUES ('Pear'       , 7   , 'img-Pear           ');
+INSERT INTO list VALUES ('Raspberry'  , 25  , 'img-Raspberry      '); 
+INSERT INTO list VALUES ('Strawberry' , 40  , 'img-Strawberry     ');
